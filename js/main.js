8c6c4608cdbcf3e44acbbe20566eb4678868cc7c
Fedotov Aleksey <far@rio-soft.ru>
2021-06-09 08:33:05 +0500

    project to my git

--- js/main.js
+++ js/main.js
@@ -1,3 +1,289 @@
+const portfolio = [
+  {
+    link: '../pages/solid_sign.html',
+    img: '../img/portfolio/solid_sign/preview.jpg',
+    img_desc: 'Новогодние наборы',
+    title: 'ТВЕРДЫЙ ЗНАК',
+    description: 'Разработка дизайна упаковки подарочных наборов для компании «Орехпром»',
+    mobile_title: 'Новогодние наборы',
+    category: 'sweet',
+    service: 'package-design, zero_brand',
+  },
+  {
+    link: '../pages/em.html',
+    img: '../img/portfolio/Em/preview.jpg',
+    img_desc: 'Детский набор «Ем!»',
+    title: 'ЕМ!',
+    description: 'Разработка дизайна упаковки детских подарочных наборов для компании «Орехпром»',
+    mobile_title: 'Детский набор «Ем!»',
+    category: 'chidren, sweet',
+    service: 'zero_brand, brand-character, package-design',
+  },
+  {
+    link: '../pages/hotStuff.html',
+    img: '../img/portfolio/hot_stuff/preview.jpg',
+    img_desc: 'Горячая штучка! Новый год 2020',
+    title: 'ГОРЯЧАЯ ШТУЧКА',
+    description: 'Разработка новогодней промо-упаковки бренда «Горячая штучка!» для компании «Abi Product»',
+    mobile_title: 'Горячая штучка! Новый год 2020',
+    category: 'meet',
+    service: 'package-design, brand-character, promo-pack',
+  },
+  {
+    link: '../pages/brauni.html',
+    img: '../img/portfolio/brauni/preview.jpg',
+    img_desc: 'Брауни',
+    title: 'БРАУНИ, FARETTI',
+    description: 'РАЗРАБОТКА ДИЗАЙНА УПАКОВКИ ТОРТА «БРАУНИ» КОМПАНИИ FARETTI',
+    mobile_title: 'Брауни',
+    category: 'sweet, bread',
+    service: 'zero_brand, package-design, sku, verbal, photo',
+  },
+  {
+    link: '../pages/mrammi.html',
+    img: '../img/portfolio/mrammi/preview.jpg',
+    img_desc: 'Мрамми',
+    title: 'МРАММИ, FARETTI',
+    description: 'РАЗРАБОТКА НЕЙМА И АЙДЕНТИКИ ТОРТА «МРАММИ» КОМПАНИИ FARETTI',
+    mobile_title: 'Мрамми',
+    category: 'sweet, bread',
+    service: 'package-design, zero_brand, sku, verbal',
+  },
+  {
+    link: '../pages/gedonia.html',
+    img: '../img/portfolio/Gedonia/preview.jpg',
+    img_desc: 'Gedonia',
+    title: 'GEDONIA',
+    description: 'Разработка нового бренда сиропов и морсов',
+    mobile_title: 'Gedonia',
+    category: 'drink',
+    service: 'package-design, zero_brand, sku, verbal',
+  },
+  {
+    link: '../pages/gusto-di-roma.html',
+    img: '../img/portfolio/Gusto/preview.jpg',
+    img_desc: 'Gusto Di Roma',
+    title: 'Gusto Di Roma',
+    description: 'Разработка бренда для нового продукта собственного торговой марки компании «Магнит»',
+    mobile_title: 'Gusto Di Roma',
+    category: 'bakaleya, ctm',
+    service: 'zero_brand, package-design, sku, verbal',
+  },
+  {
+    link: '../pages/lapika.html',
+    img: '../img/portfolio/Lappika/preview.jpg',
+    img_desc: 'Lappika',
+    title: 'LAPPIKA',
+    description: 'Вывод нового бренда детских кремов для компании "Диарси"',
+    mobile_title: 'Lappika',
+    category: 'no-food, medicine',
+    service: 'zero_brand, package-design, sku, brand-character',
+  },
+  {
+    link: '../pages/oltermani.html',
+    img: '../img/portfolio/Oltermanni/preview.jpg',
+    img_desc: 'Oltermanni',
+    title: 'OLTERMANNI',
+    description: 'Разработка дизайна упаковки тертого сыра Oltermanni для компании «Valio»',
+    mobile_title: 'Oltermanni',
+    category: 'milk',
+    service: 'package-design, verbal',
+  },
+  {
+    link: '../pages/tomatti.html',
+    img: '../img/portfolio/Tomatti/preview.jpg',
+    img_desc: 'Tomatti',
+    title: 'TOMATTI',
+    description: 'Разработка нового бренда томатной пасты для компании «Эрконпродукт»',
+    mobile_title: 'Tomatti',
+    category: 'bakaleya',
+    service: 'zero_brand, package-design, sku, verbal',
+  },
+  {
+    link: '../pages/valio.html',
+    img: '../img/portfolio/valio/preview.jpg',
+    img_desc: 'Valio – для перекуса и бутербродный',
+    title: 'VALIO',
+    description: 'Разработка системы дизайна упаковки новых сыров «Для перекуса» и «Бутербродный»',
+    mobile_title: 'Valio – для перекуса и бутербродный',
+    category: 'milk',
+    service: 'package-design, verbal',
+  },
+  {
+    link: '../pages/atlet.html',
+    img: '../img/portfolio/atlet/preview.jpg',
+    img_desc: 'Valio Atleet',
+    title: 'ATLEET',
+    description: 'РЕДИЗАЙН УПАКОВКИ СЫРА «ATLEET» ДЛЯ КОМПАНИИ VALIO',
+    mobile_title: 'Valio Atleet',
+    category: 'milk',
+    service: 'redesign-pack'
+  },
+  {
+    link: '../pages/valio_yellow.html',
+    img: '../img/portfolio/valio_Yellow/preview.jpg',
+    img_desc: 'Valio Желтые сыры',
+    title: 'VALIO',
+    description: 'Редизайн упаковки линейки желтых сыров для компании Valio',
+    mobile_title: 'Valio Желтые сыры',
+    category: 'milk',
+    service: 'redesign-pack, verbal',
+  },
+  {
+    link: '../pages/bulmeni.html',
+    img: '../img/portfolio/bulmeni/preview.jpg',
+    img_desc: 'Бульмени',
+    title: 'БУЛЬМЕНИ',
+    description: 'Разработка дизайна упаковки нового продукта для компании «Abi Product»',
+    mobile_title: 'Бульмени',
+    category: 'meet',
+    service: 'package-design',
+  },
+  {
+    link: '../pages/viola.html',
+    img: '../img/portfolio/viola/preview.jpg',
+    img_desc: 'Valio Французское избранное и копченый лосось',
+    title: 'VIOLA',
+    description: 'РАЗРАБОТКА ДИЗАЙНА УПАКОВКИ НОВЫХ ВКУСОВ ПЛАВЛЕНЫХ СЫРОВ VIOLA – «ФРАНЦУЗСКОЕ ИЗБРАННОЕ» И «КОПЧЕНЫЙ ЛОСОСЬ» - ДЛЯ КОМПАНИИ VALIO',
+    mobile_title: 'Valio Французское избранное и копченый лосось',
+    category: 'milk',
+    service: 'package-design',
+  },
+  {
+    link: '../pages/kuban_children.html',
+    img: '../img/portfolio/kuban_children_juice/preview.jpg',
+    img_desc: 'Детские соки «Дары Кубани»',
+    title: 'ДЕТСКИЕ СОКИ «ДАРЫ КУБАНИ»',
+    description: 'Редизайн упаковки детских соков «Дары Кубани» для компании ЮСК',
+    mobile_title: 'Детские соки «Дары Кубани»',
+    category: 'chidren, drink',
+    service: 'redesign-pack, brand-character',
+  },
+  {
+    link: '../pages/kuban_juice.html',
+    img: '../img/portfolio/kuban_juice/preview.jpg',
+    img_desc: 'Соки «Дары Кубани»',
+    title: 'Соки «Дары Кубани»',
+    description: 'Редизайн упаковки соков «Дары Кубани» для компании ЮСК',
+    mobile_title: 'Соки «Дары Кубани»',
+    category: 'drink',
+    service: 'redesign-pack',
+  },
+  {
+    link: '../pages/kellogs.html',
+    img: '../img/portfolio/kellogs/preview.jpg',
+    img_desc: 'Единороги',
+    title: 'KELLOGG’S UNICORN',
+    description: 'Разработка промо-упаковки сухих завтраков Unicorn для компании Kellogg’s',
+    mobile_title: 'Единороги',
+    category: 'bakaleya',
+    service: 'promo-pack, package-design',
+  },
+  {
+    link: '../pages/chebureshki.html',
+    img: '../img/portfolio/chebureshki/preview.jpg',
+    img_desc: 'Жаренки',
+    title: 'ЧЕБУРЕШКИ',
+    description: 'Редизайн упаковки флагманской линейки бренда «Жаренки» для компании «Морозко»',
+    mobile_title: 'Жаренки',
+    category: 'meet',
+    service: 'redesign-pack',
+  },
+  {
+    link: '../pages/komplivite.html',
+    img: '../img/portfolio/complivite/preview.jpg',
+    img_desc: 'Компливит «Триместрум»',
+    title: 'ТРИМЕСТРУМ',
+    description: 'Редизайн упаковки витаминного комплекса для бренда «Компливит»',
+    mobile_title: 'Компливит «Триместрум»',
+    category: 'no-food',
+    service: 'redesign-pack, brand-character',
+  },
+  {
+    link: '../pages/mamalish.html',
+    img: '../img/portfolio/mamalish/preview.jpg',
+    img_desc: 'Мамалыш',
+    title: 'Мамалыш',
+    description: 'Вывод нового бренда детского питания для компании «Молком»',
+    mobile_title: 'Мамалыш',
+    category: 'chidren',
+    service: 'package-design, brand-character, verbal',
+  },
+  {
+    link: '../pages/bird.html',
+    img: '../img/portfolio/our_bird/preview.jpg',
+    img_desc: 'Наша птичка',
+    title: 'Наша птичка',
+    description: 'Запуск бренда для компании «Аргопредприятие «Ресурс»',
+    mobile_title: 'Наша птичка',
+    category: 'meet',
+    service: 'redesign-pack, brand-character, brand-book, rebrand, verbal',
+  },
+  {
+    link: '../pages/breadHouse_marjin.html',
+    img: '../img/portfolio/breadHouse_marjin/preview.jpg',
+    img_desc: 'Хлебный дом «Марьин»',
+    title: 'ХЛЕБНЫЙ ДОМ',
+    description: 'РЕСТАЙЛИНГ ДИЗАЙНА УПАКОВКИ РЖАНОГО ХЛЕБА «МАРЬИН» ДЛЯ КОМПАНИИ «ХЛЕБНЫЙ ДОМ»',
+    mobile_title: 'Хлебный дом «Марьин»',
+    category: 'bread',
+    service: 'restyle',
+  },
+  {
+    link: '../pages/breadHouse_rebrand.html',
+    img: '../img/portfolio/breadHouse_rebrand/preview.jpg',
+    img_desc: 'Хлебный дом',
+    title: 'ХЛЕБНЫЙ ДОМ',
+    description: 'РЕДИЗАЙН УПАКОВКИ ЛИНЕЙКИ БАТОНОВ ДЛЯ КОМПАНИИ «ХЛЕБНЫЙ ДОМ»',
+    mobile_title: 'Хлебный дом «Ребрендинг»',
+    category: 'bread',
+    service: 'redesign-pack, photo, rebrand',
+  },
+  {
+    link: '../pages/breadHouse_stripe.html',
+    img: '../img/portfolio/breadHouse_sverdStripe/preview.jpg',
+    img_desc: 'Хлебный дом',
+    title: 'ХЛЕБНЫЙ ДОМ',
+    description: 'РЕДИЗАЙН УПАКОВКИ «СВЕРДЛОВСКОЙ ПОЛОСКИ» ДЛЯ КОМПАНИИ «ХЛЕБНЫЙ ДОМ»',
+    mobile_title: 'Хлебный дом «Свердловская полоска»',
+    category: 'bread',
+    service: 'package-design',
+  },
+  // {
+  //   //!НЕТ ИЗОБРАЖЕНИЙ
+  //   link: '../valio/index.html',
+  //   img: '../img/portfolio/breadHouse_sverdStripe/1.jpg',
+  //   img_desc: 'Хлебокомбинат Пролетарец',
+  //   title: 'ХЛЕБОКОМБИНАТ ПРОЛЕТАРЕЦ',
+  //   description: 'РЕДИЗАЙН ЛИНЕЙКИ ХЛЕБНОЙ ПРОДУКЦИИ ДЛЯ ХЛЕБОКОМБИНАТА «ПРОЛЕТАРЕЦ»',
+  //   mobile_title: 'Хлебокомбинат Пролетарец',
+  //   category: 'bread',
+  //   service: 'package-design, restyle',
+  // },
+  {
+    link: '../pages/albero.html',
+    img: '../img/portfolio/dolche_albero/preview.jpg',
+    img_desc: 'Dolce Albero',
+    title: 'DOLCE ALBERO',
+    description: 'РАЗРАБОТКА ДИЗАЙНА УПАКОВКИ ПРОДУКЦИИ СОБСТВЕННОЙ ТОРГОВОЙ МАРКИ КОМПАНИИ «ЛЕНТА»',
+    mobile_title: 'Dolce Albero',
+    category: 'bakaleya',
+    service: 'package-design',
+  },
+  {
+    link: '../pages/uvelka.html',
+    img: '../img/portfolio/uvelka/preview.jpg',
+    img_desc: 'Увелка Китайские каши',
+    title: 'УВЕЛКА',
+    description: 'РАЗРАБОТКА ДИЗАЙНА УПАКОВКИ КАШ «УВЕЛКА» ДЛЯ ВЫВОДА НА РЫНКИ КИТАЯ, ОАЭ И США',
+    mobile_title: 'Увелка Китайские каши',
+    category: 'bakaleya',
+    service: 'package-design',
+  }
+]
+
+let countPortfolioToShow = 6;
+
 var mySwiper = new Swiper('.swiper-container', {
   // Optional parameters
   loop: true,
@@ -5,6 +291,7 @@ var mySwiper = new Swiper('.swiper-container', {
   // If we need pagination
   pagination: {
     el: '.swiper-pagination',
+    clickable: true
   },
 
   // Navigation arrows
@@ -19,21 +306,128 @@ var mySwiper = new Swiper('.swiper-container', {
   },
 });
 
+let filter_btns = document.querySelectorAll('.filter_btn');
+const filterServices_btn = [];
+const filterCategory_btn = [];
+
+const handleFilter = function (e) {
+  if (activeSubCategory !== null) {
+    activeSubCategory.classList.remove('category__active')
+  }
+  
+  activeSubCategory = e.target;
+  generatePortfolio(activeSubCategory.dataset.service);
+  activeSubCategory.classList.add('category__active')
+}
+
+const setClickHandler = function () {
+  filter_btns.forEach(function(elem) {
+    elem.onclick = handleFilter;
+  })
+
+  if (activeSubCategory) {
+    activeSubCategory.classList.remove('category__active');
+  }
+  
+  generatePortfolio(filter_btns[0].dataset.service);
+  filter_btns[0].classList.add('category__active');
+  activeSubCategory = filter_btns[0];
+}
+
+const generatePortfolio = function (filter, ) {
+  const root = document.getElementById('portfolio-container');
+  root.textContent = '';
+  let portfolioList = portfolio.slice(0, countPortfolioToShow);
+  if (filter) {
+    portfolioList = portfolio.filter(function (item) {
+      if (item.category.includes(filter) || item.service.includes(filter)) {
+        return item;
+      }
+    })
+  }
+  portfolioList.forEach(function (item) {
+    const elem = document.createElement('a');
+    elem.classList.add('project__card');
+    elem.setAttribute('href', item.link);
+
+    const img_container = document.createElement('div');
+    img_container.classList.add('work__card-img');
+    const img = document.createElement('img');
+    img.setAttribute('src', item.img);
+    img.setAttribute('alt', item.img_desc);
+    img_container.append(img);
+
+    const description_container = document.createElement('div');
+    description_container.classList.add('project__card__description');
+    const description_title = document.createElement('h3');
+    description_title.textContent = item.title;
+    const description_content = document.createElement('p');
+    description_content.textContent = item.description;
+    description_container.append(description_title);
+    description_container.append(description_content);
+
+    const mobile_title = document.createElement('p');
+    mobile_title.classList.add('work__card--desc');
+    mobile_title.classList.add('nopc');
+    mobile_title.textContent = item.mobile_title;
+
+    elem.append(img_container);
+    elem.append(description_container);
+    elem.append(mobile_title);
+    root.append(elem);
+  })
+
+  // FlexMasonry.init('.grid', {
+  //   // responsive: true,
+  //   breakpointCols: {
+      
+  //     'min-width: 992px': 3,
+  //     'min-width: 768px': 2,
+  //     'min-width: 576px': 1,
+  //   },
+  //   numCols: 4,
+  // });
+
+  // try {
+  //   // bricklayer.destroy()
+  //   bricklayer = new Bricklayer(document.querySelector('.bricklayer'))
+  // } catch {
+    
+  // }
+  // var bricklayer = new Bricklayer(document.querySelector('.bricklayer'))
+
+}
+
 
 let category1 = document.querySelector('.category-service');
 let category2 = document.querySelector('.category-grade');
 
 function categoryNoActive() {
+  activeSubCategory = null;
   category1.classList.remove('category-active');
   category2.classList.remove('category-active');
+  generatePortfolio();
+  if (more) {
+    more.style.display = 'flex';
+  }
 }
 function categoryActive1() {
   category2.classList.remove('category-active');
   category1.classList.add('category-active');
+  filter_btns = category1.querySelectorAll('.filter_btn');
+  setClickHandler();
+  if (more) {
+    more.style.display = 'none';
+  }
 }
 function categoryActive2() {
   category1.classList.remove('category-active');
   category2.classList.add('category-active');
+  filter_btns = category2.querySelectorAll('.filter_btn');
+  setClickHandler();
+  if (more) {
+    more.style.display = 'none';
+  }
 }
 
 
@@ -54,23 +448,39 @@ for (let i = 15; i < noHidden.length; i++) {
   noHidden[i].classList.add('hidden');
 }
 
-
-let more = document.querySelectorAll('.more');
-
-for (var i = 0; i < more.length; i++) {
-  more[i].addEventListener('click', function () {
-    var showPerClick = 5;
-
-    var hidden = this.parentNode.querySelectorAll('div.hidden');
-    for (var i = 0; i < showPerClick; i++) {
-      if (!hidden[i]) return this.outerHTML = "";
-      hidden[i].classList.remove('hidden');
-    }
-  });
+let more = document.getElementById('more_btn'); //document.querySelectorAll('.more');
+if (more) {
+  more.addEventListener('click', function() {
+    countPortfolioToShow = portfolio.length;
+    generatePortfolio();
+    // FlexMasonry.destroyAll()
+    // FlexMasonry.init('.grid', options = {
+    //   responsive: true,
+    //   breakpointCols: {
+    //     'min-width: 992px': 3,
+    //     'min-width: 640px': 2,
+    //     'min-width: 320px': 1,
+    // },
+    //   numCols: 2});
+  })
 }
 
 
+// for (var i = 0; i < more.length; i++) {
+//   more[i].addEventListener('click', function () {
+//     var showPerClick = 5;
+
+//     var hidden = this.parentNode.querySelectorAll('div.hidden');
+//     for (var i = 0; i < showPerClick; i++) {
+//       if (!hidden[i]) return this.outerHTML = "";
+//       hidden[i].classList.remove('hidden');
+//     }
+//   });
+// }
+
+
 let nav = document.querySelector('.nav');
+console.log('nav', nav)
 let iconClose = document.querySelector('.nav-close');
 
 function menu() {
@@ -134,3 +544,17 @@ document.onscroll = function () {
     }
   }
 }
+
+const showAllLogoHandler = () => {
+  logoBlock.classList.toggle('client-slider-expanded');
+  if(!logoBlock.classList.value.includes('expanded')) {
+    showLogoBtn.textContent = 'Показать все';
+    logoBlock.scrollIntoView(0);
+  } else {
+    showLogoBtn.textContent = 'Скрыть';
+  }  
+}
+
+const showLogoBtn = document.getElementById('showAllLogo');
+showLogoBtn.addEventListener('click', showAllLogoHandler);
+const logoBlock = document.getElementById('clients-logo');
